614210da81ae04f1088d50abb9f08c6de1ab0568
Joseph Noblett <noblettjoe@gmail.com>
2024-08-29 10:34:20 -0400

    knex migation and seed file complete

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,32 @@
+/**
+  * @param { import("knex").Knex } knex
+  * @returns { Promise<void> }
+  */
+
 exports.up = function (knex) {
   // DO YOUR MAGIC
+  return knex.schema.createTable("cars", tbl => {
+    tbl.increments();
+    tbl.text("vin")
+      .unique()
+      .notNullable();
+    tbl.text("make")
+      .notNullable();
+    tbl.text("model")
+      .notNullable();
+    tbl.integer("mileage")
+      .notNullable()
+    tbl.text("title");
+    tbl.text("transmission")
+  });
 };
 
+ /**
+   * @param { import("knex").Knex } knex
+   * @returns { Promise<void> }
+   */
+
+
 exports.down = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.dropTableIfExists("cars");
 };
--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,15 @@
-// STRETCH
+/**
+   * @param { import("knex").Knex } knex
+   * @returns { Promise<void> }
+   */
+exports.seed = async function(knex) {
+  //Deletes ALL existing entries
+  await knex('cars').truncate()
+  await knex('cars').insert([
+    { vin: "1560hdfy15ahy651", make: "Ford", model: "f-150", mileage: 123_123},
+    { vin: "htgakhta35195aka", make: "Lexis", model: "i3", mileage: 51531},
+    { vin: "5hj15kaih85h8ai5", make: "Ford", model: "Transit", mileage: 999_531},
+    { vin: "ajg83ajta3jtajt8", make: "Toyota", model: "Camery", mileage: 489_142, title: "title_01", transmission: "Midnight Proformance"},
+  ]);
+};
+
